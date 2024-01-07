35c8e911f8c6f424c0779c345878c2f83238c7bd
Harsh Tuwar <tuwarharsh08@gmail.com>
2024-01-07 17:53:45 -0500

    general buttons on the navbar created

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,6 +4,8 @@ import { PRODUCT_CATEGORIES } from '@/config';
 import { Button } from './ui/button';
 import { ChevronDown } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
 
 type Category = typeof PRODUCT_CATEGORIES[number];
 
@@ -30,6 +32,40 @@ const NavItem = ({
 					})} />
 				</Button>
 			</div>
+			{isOpen ? (
+				<div className={cn("absolute inset-x-0 top-full text-sm text-muted-foreground", {
+					"animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen
+				})}>
+					<div className='absolute inset-0 top-1/2 bg-white shadow' aria-hidden='true' /> 
+
+					<div className='relative bg-white'>
+						<div className="mx-auto max-w-7xl px-8">
+							<div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+								<div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+									{category.featured.map((feature) => (
+										<div className='group relative text-base sm:text:sm' key={feature.name}>
+											<div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
+												<Image
+													src={feature.imageSrc}
+													alt='product category image'
+													fill
+													className='object-cover object-center'
+												/>
+											</div>
+
+											<Link href={feature.href} className='mt-6 block font-medium text-gray-600'>
+												{feature.name}
+											</Link>
+
+											<p className='mt-1' aria-hidden='true'>Show now</p>
+										</div>
+									))}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			) : null}
 		</div>
 	)
 }
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,34 @@
 'use client';
 
 import { PRODUCT_CATEGORIES } from '@/config';
-import { useState } from 'react';
+import { ElementRef, useEffect, useRef, useState } from 'react';
 import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 
 const NavItems = () => {
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
+	const navRef = useRef<HTMLDivElement | null>(null);
 
 	const isAnyOpen = activeIndex !== null;
 
+	useOnClickOutside(navRef, () => setActiveIndex(null));
+
+	useEffect(() => {
+		const handler = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') {
+				setActiveIndex(null);
+			}
+		}
+
+		document.addEventListener('keydown', handler);
+
+		return () => {
+			document.removeEventListener('keydown', handler);
+		}
+	}, []);
+
 	return (
-		<div className='flex gap-4 h-full'>
+		<div className='flex gap-4 h-full' ref={navRef}>
 			{PRODUCT_CATEGORIES.map((category, i) => {
 				const handleOpen = () => {
 					if (activeIndex === i) {
@@ -22,13 +40,15 @@ const NavItems = () => {
 
 				const isOpen = i === activeIndex;
 
-				return (<NavItem
-					category={category}
-					handleOpen={handleOpen}
-					isOpen={isOpen}
-					key={category.value}
-					isAnyOpen={isAnyOpen}
-				/>)
+				return (
+					<NavItem
+						category={category}
+						handleOpen={handleOpen}
+						isOpen={isOpen}
+						key={category.value}
+						isAnyOpen={isAnyOpen}
+					/>
+				)
 			})}
 		</div>
 	)
