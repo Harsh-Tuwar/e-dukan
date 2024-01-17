ec664f2235a9d042dbab9011baab70dd1b5a4d09
Harsh Tuwar <tuwarharsh08@gmail.com>
2024-01-17 10:43:23 -0500

    navbar implemeneteD

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link';
+import { cookies } from 'next/headers';
 
-import { Icons } from './Icons';
-
-import NavItems from './NavItems';
-import MaxWidthWrapper from './MaxWidthWrapper';
-import { buttonVariants } from './ui/button';
 import Cart from './Cart';
+import { Icons } from './Icons';
+import NavItems from './NavItems';
+import UserAccountNav from './UserAccountNav';
+import MaxWidthWrapper from './MaxWidthWrapper';
 
-const Navbar = () => {
-	const user = null;
+import { buttonVariants } from './ui/button';
+
+import { getServerSideUser } from '@/lib/payload-utils';
+
+const Navbar = async () => {
+	const nextCookies = cookies();
+	const { user } = await getServerSideUser(nextCookies);
 
 	return (
 		<div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
@@ -34,7 +39,17 @@ const Navbar = () => {
 
 									{!user && <span className='h-6 w-px bg-gray-200' aria-hidden='true'></span>}
 
-									{!user && <Link href='/sign-up' className={buttonVariants({ variant: 'ghost' })}>Create account</Link>}
+									{user ? (
+										<UserAccountNav user={user} />
+									) : (
+										<Link
+											href='/sign-up'
+											className={buttonVariants({
+												variant: 'ghost',
+											})}>
+											Create account
+										</Link>
+									)}
 
 									{user && <span className='h-6 w-px bg-gray-200' aria-hidden='true'></span>}
 
